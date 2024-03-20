93c4c369456df6fb7c05d062c584fc9c9266697c
japago25andes <j.pardog@uniandes.edu.co>
2024-03-20 10:18:59 -0500

    uso de rutas con variables mas de una variable y tipo

--- hello.py
+++ hello.py
@@ -8,7 +8,18 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
-
-@app.route('/user/<name>')
-def hello(name):
-    return f"Hello, {name}!"
+#string
+#int
+#float
+#path === para poder recibir cadenas de caracteres con caracteres especiales
+#uuid
+@app.route('/hello')
+@app.route('/hello/<name>')
+@app.route('/hello/<name>/<int:age>')
+def hello(name = None, age = None):
+    if name is None and age is None:
+        return "Hello, World!"
+    elif age is None:
+        return f"Hello, {name}!"
+    else:
+        return f"Hello, {name}  {age * 5}!"
